09bc5e7d2e939890b56533894d786d804b847fe5
satyam prajapati <satyamprajapati1979@gmail.com>
2024-12-03 19:42:36 +0530

    intilazing project and adding events card

---
+++ script.js
@@ -0,0 +1,19 @@
+// Typed.js Initialization
+const typed = new Typed('#typed', {
+    strings: [
+      "Softskill Accelerator", 
+      "Placement Oriented Session", 
+      "Open Source Tools", 
+      "Webathon'2024", 
+      "Strategies for Successful Placement",
+      "Expert Talk",
+      "Spring Boot Workshop",
+      "Byte Quest",
+      "Codezest'24",
+      "Placement Talks"
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+  });
+  
